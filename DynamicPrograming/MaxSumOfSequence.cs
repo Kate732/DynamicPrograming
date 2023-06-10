e1b2dc537b3345fa7c6666f4186ad8414fc47613
Kate_M <kate.maistrenko22@bathspa.ac.uk>
2023-06-10 19:31:41 +0100

    Add project files.

---
+++ DynamicPrograming/MaxSumOfSequence.cs
@@ -0,0 +1,48 @@
+﻿namespace Kate.DynamicPrograming
+{
+	public static class MaxSumOfSequence
+	{
+		public static double CalculateMaxSumOfSequence(List<double> list)
+		{
+			double sumOfAnswer = 0;
+			double maxSumConnectedToCurrentItem = 0;
+			bool isConnectedToSum = true;
+			foreach (double item in list)
+			{
+				if (item > 0)
+				{
+					if (isConnectedToSum)
+					{
+						sumOfAnswer += item;
+						maxSumConnectedToCurrentItem += item;
+					}
+					else
+					{
+						if (maxSumConnectedToCurrentItem + item >= sumOfAnswer)
+						{
+							sumOfAnswer = item + maxSumConnectedToCurrentItem;
+							isConnectedToSum = true;	
+						}
+						else
+						{
+							maxSumConnectedToCurrentItem += item;
+						}
+					}
+				}
+				else
+				{
+					if (maxSumConnectedToCurrentItem + item < 0)
+					{
+						maxSumConnectedToCurrentItem = 0;
+					}
+					else
+					{
+						maxSumConnectedToCurrentItem += item;
+					}
+					isConnectedToSum = false;
+				}
+			}
+			return sumOfAnswer;
+		}
+	}
+}
---
+++ TestCalculateMaxSumOfSequence/UnitTest1.cs
@@ -0,0 +1,44 @@
+using Kate.DynamicPrograming;
+
+namespace TestCalculateMaxSumOfSequence
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestEmptyList()
+		{
+			List<double> origin = new List<double> {  };
+			double expected = 0;
+			Assert.AreEqual(expected, MaxSumOfSequence.CalculateMaxSumOfSequence(origin));
+		}
+		[TestMethod]
+		public void TestAllNegativeNumbers()
+		{
+			List<double> origin = new List<double> { -1, -25, -12 };
+			double expected = 0;
+			Assert.AreEqual(expected, MaxSumOfSequence.CalculateMaxSumOfSequence(origin));
+		}
+		[TestMethod]
+		public void TestAllPositiveNumbers()
+		{
+			List<double> origin = new List<double> { 1, 2, 3, 4, 5 };
+			double expected = 15;
+			Assert.AreEqual(expected, MaxSumOfSequence.CalculateMaxSumOfSequence(origin));
+		}
+		[TestMethod]
+		public void TestNegativeAndPositiveNumbersAnswerIsInTheEnd()
+		{
+			List<double> origin = new List<double> { 5, -1, -3, 6, -25, 8 };
+			double expected = 8;
+			Assert.AreEqual(expected, MaxSumOfSequence.CalculateMaxSumOfSequence(origin));
+		}
+		[TestMethod]
+		public void TestNegativeAndPositiveNumbersAnswerIsInTheMiddle()
+		{
+			List<double> origin = new List<double> { 5, -1, -3, 6, -25, 2 };
+			double expected = 7;
+			Assert.AreEqual(expected, MaxSumOfSequence.CalculateMaxSumOfSequence(origin));
+		}
+	}
+}
